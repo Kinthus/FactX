15df1fc5bc4426b9900247a055403c5b499c9e24
Kinthus <arithaskinthusan@gmail.com>
2026-07-15 12:29:02 +0530

    initial commit

---
+++ app/src/main/java/com/example/factx/activity_main.java
@@ -0,0 +1,28 @@
+package com.example.factx;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_main extends AppCompatActivity {
+
+    Button btnGetStarted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+
+        btnGetStarted.setOnClickListener(v -> {
+
+            Intent intent = new Intent(activity_main.this, activity_login.class);
+            startActivity(intent);
+
+        });
+    }
+}
