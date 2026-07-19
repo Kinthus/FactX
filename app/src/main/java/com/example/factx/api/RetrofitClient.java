868ed691bf79b3f0ca8a2e15d117b35293542342
TMathu <tmathumitha21@gmail.com>
2026-07-19 15:04:55 +0530

    database

---
+++ app/src/main/java/com/example/factx/api/RetrofitClient.java
@@ -0,0 +1,24 @@
+package com.example.factx.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    // Android Emulator பயன்படுத்தினால்
+    private static final String BASE_URL = "http://10.0.2.2:8000/";
+
+    private static Retrofit retrofit;
+
+    public static Retrofit getClient() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
