32a1b86a1a69b8c55a8a516734bf97bbc893e88b
VTharshi <tharshikakoneswaran201@gmail.com>
2026-08-10 16:49:43 +0530

    update898

--- app/src/main/java/com/example/factx/activity_logout.java
+++ app/src/main/java/com/example/factx/activity_logout.java
@@ -3,41 +3,51 @@ package com.example.factx;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_logout extends AppCompatActivity {
 
-    Button btnLogout, btnCancel;
+    Button btnLogout;
+    Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_logout);
 
         btnLogout = findViewById(R.id.btnLogout);
         btnCancel = findViewById(R.id.btnCancel);
 
-        // Logout
-        btnLogout.setOnClickListener(v -> {
 
-            Toast.makeText(
-                    activity_logout.this,
-                    "Logged Out Successfully",
-                    Toast.LENGTH_SHORT
-            ).show();
+        // ==========================================
+        // LOGOUT
+        // ==========================================
+
+        btnLogout.setOnClickListener(v -> {
 
             Intent intent = new Intent(
                     activity_logout.this,
-                    activity_login.class);   // உங்கள் Login Activity name வேறு என்றால் அதை மாற்றுங்கள்
+                    activity_login.class
+            );
+
+            intent.setFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK
+            );
 
             startActivity(intent);
+
             finish();
 
         });
 
-        // Cancel
+
+        // ==========================================
+        // CANCEL
+        // ==========================================
+
         btnCancel.setOnClickListener(v -> {
 
             finish();
